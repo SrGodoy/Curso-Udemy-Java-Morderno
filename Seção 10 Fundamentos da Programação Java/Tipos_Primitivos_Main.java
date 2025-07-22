5833b0f225b2c0e079f40914dc6e84660a61d2cd
SrGodoy <luccasgodoy2012@gmail.com>
2025-07-21 23:13:34 -0300

    Teste Variaveis e Tipos Primitivos

---
+++ Seção 10 Fundamentos da Programação Java/Tipos_Primitivos_Main.java
@@ -0,0 +1,21 @@
+public class Tipos_Primitivos_Main {
+    public static void main(String[] args) {
+        byte idade = 25;
+        short ano = 2025;
+        int populacaoCidade = 500000;
+        long populacaoMundial = 7800000000000L;
+        float altura = 1.75f;
+        double salario = 4500.50;
+        boolean estudante = true;
+        char inicialNome = 'L';
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Ano: " + ano);
+        System.out.println("População da cidade: " + populacaoCidade);
+        System.out.println("População Mundial: " + populacaoMundial);
+        System.out.println("Altura: " + altura);
+        System.out.println("Salario: " + salario);
+        System.out.println("É estudante? : " + estudante);
+        System.out.println("Inicial do Nome: " + inicialNome);
+    }
+}
---
+++ Seção 10 Fundamentos da Programação Java/VariaveisMain.java
@@ -0,0 +1,6 @@
+public class VariaveisMain {
+    public static void main (String[] args) {
+        int farinha = 2;
+        System.out.println("A Receita requer " + farinha + "xícaras de farinha. ");
+    }
+}
