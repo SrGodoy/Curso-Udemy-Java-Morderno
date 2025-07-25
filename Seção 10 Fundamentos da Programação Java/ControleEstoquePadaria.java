0d42a7182989968ec8bb0d0ad58d14f691395b4f
SrGodoy <luccasgodoy2012@gmail.com>
2025-07-25 06:04:45 -0300

    Concluido o primeiro exercicio

---
+++ Seção 10 Fundamentos da Programação Java/ControleEstoquePadaria.java
@@ -0,0 +1,11 @@
+public class ControleEstoquePadaria {
+public static void main (String[] args ){
+       int quantidadeFarinha = 10;
+       int quantidadeAcucar = 10 ;
+
+       System.out.println ("A Quantidade de Açucar é " + quantidadeAcucar + " e a quantidade de Farinha é " +quantidadeFarinha);
+
+
+
+}
+}
