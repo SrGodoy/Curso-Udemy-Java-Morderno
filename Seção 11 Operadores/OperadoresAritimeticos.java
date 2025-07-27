dbdab6638dad868dd7d848efa90e0e02e4f8332b
SrGodoy <luccasgodoy2012@gmail.com>
2025-07-27 04:40:10 -0300

    Atualizando .gitignore

---
+++ Seção 11 Operadores/OperadoresAritimeticos.java
@@ -0,0 +1,11 @@
+public class OperadoresAritimeticos {
+    public static void main (String[] args) {
+        int a = 10;
+        int b = 2;
+        System.out.println(a + b); // Saída: 12
+        System.out.println(a - b); // Saída: 8
+        System.out.println(a * b); // Saída: 20
+        System.out.println(a / b); // Saída: 5
+        System.out.println(a % b); // Saída: 0
+    }
+}
---
+++ Seção 11 Operadores/OperadoresDeComparacao.java
@@ -0,0 +1,13 @@
+public class OperadoresDeComparacao {
+    public static void main (String [] args ){
+        int a = 10;
+        int b = 20;
+        System.out.println(a == b); // Saída: false
+        System.out.println(a != b); // Saída: true
+        System.out.println(a > b); // Saída: false
+        System.out.println(a < b); // Saída: true
+        System.out.println(a >= b); // Saída: false
+        System.out.println(a <= b); // Saída: true
+
+    }
+}
