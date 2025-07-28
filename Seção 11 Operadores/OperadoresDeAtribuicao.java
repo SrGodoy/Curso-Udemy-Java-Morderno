c63e7832712df4add6089227aacb1944d49f326f
SrGodoy <luccasgodoy2012@gmail.com>
2025-07-28 04:09:42 -0300

    Estudo de Operadores Java

---
+++ Seção 11 Operadores/OperadoresDeAtribuicao.java
@@ -0,0 +1,9 @@
+public class OperadoresDeAtribuicao {
+    public static void main(String [] args) {
+        int a = 10;
+        a += 2; // Equivalente a a = a + 2;
+        System.out.println(a); // Saída: 12
+
+
+    }
+}
---
+++ Seção 11 Operadores/OperadoresLogicos.java
@@ -0,0 +1,11 @@
+public class OperadoresLogicos {
+    public static void main(String [] args) {
+
+        int a = 10;
+        int b = 20;
+        boolean c = true;
+        System.out.println(a < b && c); // Saída: true
+        System.out.println(a > b || c); // Saída: true
+        System.out.println(!c); // Saída: false
+    }
+}
