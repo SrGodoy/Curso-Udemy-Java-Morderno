86db945dd7a05be294dc1276cc8220cf4ef3979f
SrGodoy <luccasgodoy2012@gmail.com>
2025-08-01 02:27:01 -0300

    Seção 12 finalizada

--- Seção 12 Controle de Fluxo/exercicio.java
+++ Seção 12 Controle de Fluxo/exercicio.java
@@ -39,8 +39,61 @@ import java.util.Scanner;
             }
             System.out.println(feedback);
 
-            sc.close();
+
+            //Exercicio complementar 1: Verificador de mês
+            int mes1 = 3;
+            String tipoDeMes = switch (mes1) {
+                case 1 -> "Janeiro";
+                case 2 -> "Fevereiro";
+                case 3 -> "Março";
+                case 4 -> "Abril";
+                case 5 -> "Maio";
+                case 6 -> "Junho";
+                case 7 -> "Julho";
+                case 8 -> "Agosto";
+                case 9 -> "Setembro";
+                case 10 -> "Outubro";
+                case 11 -> "Novembro";
+                case 12 -> "Dezembro";
+                default -> "Numero errado";
+            };
+            System.out.println(tipoDeMes);
+
+            //Exercício Complementar 2: Número de Dias no Mês
+
+            String mes2 = "Abril";
+
+            String tipoDeMes2 = switch (mes2) {
+                case "Janeiro", "Março", "Maio", "Julho", "Agosto" ,"Outubro", "Dezembro"
+                        -> "Meses com 31 dias";
+                case "Abril", "Junho", "Setembro", "Novembro" -> "Meses com 30 dias";
+                case "Fevereiro" -> "Mes com 28 Dias";
+                default -> "Mes Desconhecido";
+            };
+            System.out.println(tipoDeMes2);
+
+            //Exercício Complementar 3: Verificação de Aprovação Simplificada
+            int nota1 = 10;
+            int nota2 = 7;
+            if (((nota1 + nota2) / 2) >= 6){
+                System.out.println("O Aluno esta aprovado");
+            }else {
+                System.out.println("O Aluno foi reprovado");
             }
+
+            //Exercício Complementar 4: Verificação de Aprovação com Base em Notas e Presença
+            int nota11 = 10;
+            int nota22 = 8;
+            int frequencia = 80;
+
+            if (((nota11 + nota22) / 2) >= 6 && frequencia >= 75){
+                System.out.println("O Aluno esta aprovado");
+            }else{
+                System.out.println("O Aluno foi reprovado");
+            }
+
+
+        }
         }
 
 
