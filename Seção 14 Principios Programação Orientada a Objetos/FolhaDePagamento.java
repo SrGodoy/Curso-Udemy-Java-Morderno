4533652bccadf9d3fe3be083fa71657110fc93c0
SrGodoy <luccasgodoy2012@gmail.com>
2025-08-05 23:26:56 -0300

    Package Java Conceitos Basicos

---
+++ Seção 14 Principios Programação Orientada a Objetos/FolhaDePagamento.java
@@ -0,0 +1,7 @@
+import empresa.dados.Funcionario;
+public class FolhaDePagamento {
+    public static void main(String[] args) {
+
+        Funcionario funcionario = new Funcionario("Maria", 6200.00);
+    }
+}
