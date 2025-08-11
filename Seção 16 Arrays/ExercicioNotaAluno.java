d83098189f8f9abe7e5a72211bb72bd646dd59d9
SrGodoy <luccasgodoy2012@gmail.com>
2025-08-10 23:28:34 -0300

    Inicio da Seção 16 Arrays

---
+++ Seção 16 Arrays/ExercicioNotaAluno.java
@@ -0,0 +1,11 @@
+public class ExercicioNotaAluno {
+    public static void main(String[] args) {
+
+
+        double [] arrayNota = new double[]{5.7, 6.8, 9, 10, 2}
+
+        for (double nota : arrayNota){
+            System.out.println(nota);
+        }
+    }
+}
