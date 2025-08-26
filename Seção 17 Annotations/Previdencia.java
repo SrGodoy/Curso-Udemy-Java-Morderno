a3fe6d8954886f6b0a4b335f27fbed23e6b18e17
SrGodoy <luccasgodoy2012@gmail.com>
2025-08-25 21:44:05 -0300

    Annotations Parte 1

---
+++ Seção 17 Annotations/Previdencia.java
@@ -0,0 +1,22 @@
+class InformaRegras {
+
+    @Deprecated // <-- Atenção aqui
+    public void mostrarRegrasParaAposentadoria() {
+        System.out.println("Regras para Aposentadoria");
+    }
+    public void mostrarNovasRegrasParaAposentadoria() {
+        System.out.println("NOVAS Regras para Aposentadoria");
+    }
+}
+
+public class Previdencia {
+    public static void main(String[] args) {
+        InformaRegras regras = new InformaRegras();
+
+        //Uso do metodo obsoleto (deprecated)
+        //regras.mostrarRegrasParaAposentadoria();
+
+        //Uso do novo método
+        regras.mostrarNovasRegrasParaAposentadoria();
+    }
+}
---
+++ Seção 17 Annotations/TestAnnotations.java
@@ -0,0 +1,23 @@
+class Animal {
+        public void fazerSom() {
+            System.out.println("O animal faz um som");
+
+    }
+}
+
+class Cachorro extends Animal {
+
+    @Override
+    public void fazerSom() {
+        System.out.println("O cachorro late");
+    }
+
+}
+
+public class TestAnnotations{
+    public static void main(String[] args) {
+        Cachorro dog = new Cachorro();
+        dog.fazerSom();
+
+    }
+}
