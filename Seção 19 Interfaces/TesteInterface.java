2f2a65ef2f8f5e55be2dd11a6af3361f7c67a331
SrGodoy <luccasgodoy2012@gmail.com>
2025-09-17 02:33:50 -0300

    Introdução a Interfaces

---
+++ Seção 19 Interfaces/TesteInterface.java
@@ -0,0 +1,32 @@
+interface Animal {
+    void fazerSom(); //método abstrato
+}
+
+//Classe Cachorro que implementa a interface animal
+class Cachorro implements Animal {
+
+    @java.lang.Override
+    public void fazerSom() {
+        System.out.println("O cachorro faz: Au Au!");
+    }
+}
+
+// Classe Gato que implementa a interface animal
+ class Gato implements Animal {
+
+    @java.lang.Override
+    public void fazerSom() {
+        System.out.println("O gato faz: Miau Miau!");
+    }
+}
+
+public class TesteInterface{
+    public static void main(String[] args) {
+
+        Cachorro cachorro = new Cachorro();
+        cachorro.fazerSom(); // deve imprimir au au
+
+        Gato gato = new Gato();
+        gato.fazerSom(); // deve imprimir miau maiu
+    }
+}
