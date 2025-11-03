66f6ae8a0f7c7598540ce4e3b5ee4137ccba1285
Luccas Godoy <luccasgodoy2012@gmail.com>
2025-11-03 02:40:25 -0300

    Introdução a Generics

---
+++ Seção 22 Generics/Caixa.java
@@ -0,0 +1,23 @@
+public class Caixa<T> {
+    private T coisaNaCaixa;
+
+    public void guardar(T coisa) {
+        this.coisaNaCaixa = coisa;
+    }
+
+    public T pegar(){
+        return coisaNaCaixa;
+    }
+
+    public static void main(String[] args) {
+        //Criando uma caixa para guardar Strings
+        Caixa<String> caixaDeTexto = new Caixa<>();
+
+        caixaDeTexto.guardar("Oi, Mundo");
+
+        String texto = caixaDeTexto.pegar();
+
+        System.out.println(texto); //Imprime : Oi, Mundo
+    }
+
+}
